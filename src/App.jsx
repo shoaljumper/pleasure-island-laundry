382d43063e136e710186666629e38d4d463a69c7
shoaljumper <shoaljumper@proton.me>
2025-07-07 20:31:16 -0400

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-@@ .. @@
-           <div className="absolute inset-0 bg-gradient-to-b from-ocean-900/80 via-ocean-800/60 to-transparent"></div>
-           <img 
--            src="https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-+            src="/beach-header.jpg"
-             alt="Beautiful Carolina Beach coastline" 
-             className="w-full h-full object-cover"
-           />
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
